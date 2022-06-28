1562920324b6a1bc8dfb94f5129d2b6fea9c0fab
wikicode96 <101192857+wikicode96@users.noreply.github.com>
2022-06-28 13:01:06 +0200

    This is a first version. Not finished
    I added left template and it connected to api

---
+++ script.js
@@ -0,0 +1,30 @@
+/* Input #TO DO */ 
+
+consultAPI(301)
+
+/* Connect to API */
+async function consultAPI(pokeId){
+    try {
+        const url = `https://pokeapi.co/api/v2/pokemon/${pokeId}/`;
+        const result = await fetch(url);
+        pokeJson = await result.json()
+
+        /* Parse JSON */
+        pokeName = pokeJson.name
+        pokeName = firstLetterUppercas(pokeName);
+        pokeImg = pokeJson.sprites.front_default
+
+        /* Poke Name */
+        document.getElementById('pokemon_name').innerHTML = pokeName
+        
+        /* Poke Img */
+        document.getElementById('pokemon_img').src = pokeImg
+
+    }catch (error) {
+        console.log(error)
+    }
+}
+
+function firstLetterUppercas(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
