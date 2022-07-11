e14707d68de6b8d6de646012252de21f438ff18f
Carlos Soler <101192857+wikicode96@users.noreply.github.com>
2022-07-11 21:36:57 +0200

    add turn on and turn off functions
    Functions are implemented to turn on and off this pokedex. It´s not functional

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
-/* Input #TO DO */ 
+/* Turn Off (Done)*/
+function turnedOff(){
+    document.getElementById('pokemon_name').innerHTML = '';
+    document.getElementById('write_id').innerHTML = '';
+    document.getElementById('id').innerHTML = '';
+    document.getElementById('blue_circle').style = 'filter: brightness(50%);'
+    document.getElementById('light_1').style = 'filter: brightness(50%);'
+    document.getElementById('light_2').style = 'filter: brightness(50%);'
+    document.getElementById('light_3').style = 'filter: brightness(50%);'
+    document.getElementById('pokemon_img').remove();
+}
 
-consultAPI(382)
-    
+/* Turn On */
+function turnedOn(){
+    document.getElementById('pokemon_name').innerHTML = 'Pokemon Name';
+    document.getElementById('write_id').innerHTML = 'Touch here!';
+    document.getElementById('id').innerHTML = 'ID: ';
+    document.getElementById('blue_circle').style = 'filter: brightness(100%);'
+    document.getElementById('light_1').style = 'filter: brightness(100%);'
+    document.getElementById('light_2').style = 'filter: brightness(100%);'
+    document.getElementById('light_3').style = 'filter: brightness(100%);'
 
-/* Connect to API */
+    /* Create img tag */
+    const img = document.createElement("img");
+    img.id = 'pokemon_img'
+
+    const display = document.getElementById('display')
+    display.appendChild(img)
+}
+
+/* Connect to API (Right Text)*/
 async function consultAPI(pokeId){
     try {
         const url = `https://pokeapi.co/api/v2/pokemon/${pokeId}/`;
